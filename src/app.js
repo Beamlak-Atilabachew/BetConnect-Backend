52f3a79fd436c6559a5974b184ea4009aba257f4
labba1 <beamlakatilabachew@gmail.com>
2026-04-17 17:02:39 +0300

    Add Admin Flow & Bookmarks System

--- src/app.js
+++ src/app.js
@@ -1,12 +1,28 @@
 import express from 'express';
+import authRoutes from "./routes/auth.routes.js";
+import bookmarkRoutes from "./routes/bookmark.routes.js";
+import adminRoutes from "./routes/admin.routes.js";
+import { errorHandler } from "./middleware/error.middleware.js";
+
 
 const app = express();
 
 app.use(express.json());
+
+// Routes
+app.use('/api/auth', authRoutes);
+app.use('/api/bookmarks', bookmarkRoutes);
+app.use('/api/admin', adminRoutes);
+
+// Health check
+
 app.get('/health', (req, res) => {
     res.status(200).json({ 
         status: 'success',
         message: 'Server is healthy' });
-})
+});
+
+// Error handler
+app.use(errorHandler);
 
 export default app;
